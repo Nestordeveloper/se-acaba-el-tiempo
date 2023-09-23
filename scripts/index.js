4dbf54da7cae07f336e7d122ca915fb9ae54c344
Nestor Larenas <sne.larenasortega@hotmail.com>
2023-09-23 20:23:20 -0300

    Se acaba el tiempo actividad completada

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,35 @@
 function getSecondsSinceStartOfDay() {
     return new Date().getSeconds() + 
-      new Date().getMinutes() * 60 + 
-      new Date().getHours() * 3600;
-  }
+    new Date().getMinutes() * 60 + 
+    new Date().getHours() * 3600;
+}
+
+function getHourHandAngle(time) {
+    time %= 43200;
+    return ((360 * time / 43200) + 180) % 360;
+}
+
+function getMinuteHandAngle(time) {
+    time %= 3200;
+    return ((6 * time / 60) + 180) % 360;
+}
+
+function getSecondHandAngle(time) {
+    time %= 60;
+    return ((6 * time) + 180) % 360;
+}
+
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
       
-  setInterval( function() {
-      var time = getSecondsSinceStartOfDay();
-      console.log(time);
-  }, 1000);
-  
+setInterval( function() {
+    var time = getSecondsSinceStartOfDay();
+    console.log(time);
+    hour.style.transform = 
+    `rotate(${getHourHandAngle(time)}deg)`;
+    minutes.style.transform =
+    `rotate(${getMinuteHandAngle(time)}deg)`;
+    seconds.style.transform =
+    `rotate(${getSecondHandAngle(time)}deg)`;
+}, 1000);
